85a4c1084d445e7c3206fcd62642e5c240af5270
Akash <158745842+Akash8t2@users.noreply.github.com>
2024-04-14 16:03:21 +0530

    Update start.py

--- DAXXMUSIC/utils/inline/start.py
+++ DAXXMUSIC/utils/inline/start.py
@@ -6,7 +6,7 @@ from DAXXMUSIC import app
 
 def start_panel(_):
     buttons = [
-        [
+[
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
@@ -18,17 +18,17 @@ def start_panel(_):
 
 def private_panel(_):
     buttons = [
-        [
+[
             InlineKeyboardButton(
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
-        [
+[
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
-        [
+[
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
         ],
     ]
