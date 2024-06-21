8a0825d68c0d9b7ad4655bee05a3d136e855ee98
twinkeee7 <167674604+twinkeee7@users.noreply.github.com>
2024-06-21 10:50:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ API_HASH = getenv("API_HASH")
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 # Add Owner Username without @ 
-OWNER_USERNAME = getenv("OWNER_USERNAME","iam_daxx")
+OWNER_USERNAME = getenv("OWNER_USjERNAME","iam_daxx")
 # Get Your bot username
 BOT_USERNAME = getenv("BOT_USERNAME" , "NexikoBot")
 # Don't Add style font 
